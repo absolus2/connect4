1ca43ba8a3bd0fba4e9a4cf398aa01bf183b2b1f
absolus2 <absolus24@gmail.com>
2023-01-04 15:09:18 -0400

    win conditions

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,36 +1,71 @@
 class Connect
 
   def initialize
-    @board = Hash[(1..7).map { |num| [num, Array.new(6)] }]
+    @null = "\u26d4"
+    @board = Hash[(1..7).map { |num| [num, Hash[(1..6).map { |row| [row, @null] }]] }]
     @player1 = "\u26aa"
     @player2 = "\u26ab"
   end
 
-  def place_player(column, player, count = 5)
-    return @board[column][count] = player if @board[column][count].nil?
+  def place_player(column, player, count = 6)
+    return @board[column][count] = player if @board[column][count] == @null
 
-    if count.negative?
+    if @board[column].values.all? { |item| item == player }
       puts 'Please try another column, That one is full already!'
       answer = gets.chomp.to_i
-      return place_player(answer, player, 5)
+      return place_player(answer, player, 6)
     end
-    place_player(column, player, count -= 1) unless @board[column][count].nil?
+
+    place_player(column, player, count -= 1)
   end
 
-  def victory?(count = 0)
-    @board.each do |key, value| 
-      value.each do |item| 
-        p item
+  def testing(player, vic = { v: 0, h: Hash[(1..6).map { |num| [num, 0] }], d: nil })
+    @board.each do |column, value|
+      value.each do |row, values|
+        vic[:d] = get_diagonal(column, row)
+        values == player ? vic[:v] += 1 : vic[:v] = 0
+        vic[:h][row] += 1 if values == player
+        break if win(vic, player)
       end
+      break if win(vic, player)
+    end
+    win(vic, player)
+  end
+
+  def win(vic, player)
+    return true if vic[:d].values.each.all? { |item| item == player }
+    return true if vic[:v] == 4
+    return true if vic[:h].values.any?(4)
+  end
+
+  def get_diagonal(column, value, array = { dd: [@board[column][value]], du: [@board[column][value]] })
+    count = value
+    4.times do
+      begin
+        array[:dd].push(@board[column += 1][value += 1])
+        array[:du].push(@board[column][count -= 1])
+      rescue NoMethodError
+        next
+      end
+      break if array[:du].length == 4
+    end
+    array
+  end
+
+  def display_board(row = 1, flag: false)
+    until flag
+      puts "#{@board[1][row]}#{@board[2][row]}#{@board[3][row]}#{@board[4][row]}#{@board[5][row]}#{@board[6][row]}#{@board[7][row]}"
+      row += 1
+      flag = true if row == 7
     end
   end
-  
-  def display_board
-    @board
-  end
+
 end
 
-
 bored = Connect.new
-bored.victory?
+
+
+bored.place_player(4, "\u26ab")
+bored.testing("\u26ab")
+bored.display_board
 
--- spec/connect_spec.rb
+++ spec/connect_spec.rb
@@ -2,32 +2,35 @@
 
 require_relative '../lib/connect'
 
-
 describe Connect do
-  
   subject(:game) { described_class.new }
 
   describe '#initialize' do
-
     context 'When game start' do
-
       before do
-        @board = Hash[(1..7).map { |num| [num, Array.new(6)] }]
+        @board = game.instance_variable_get(:@board)
+        @null = game.instance_variable_get(:@null)
         @player1 = game.instance_variable_get(:@player1)
         @player2 = game.instance_variable_get(:@player2)
       end
 
-      it 'should create a board(hash)' do
-        board = game.instance_variable_get(:@board)
-        expect(board).to eq(@board)
+      it 'should create a board (hash of hashes)' do
+        expect(@board).to be_a Hash
       end
 
       it 'the board should be of length 7' do
-        expect(@board.length).to eq(7)
+        result = @board.length
+        expect(result).to eq(7)
       end
 
-      it 'the board value should be nil and length of 6' do
-        expect(@board[1]).to eq([nil, nil, nil, nil, nil, nil])
+      it 'the board rows should be of lenght 6 ' do
+        row = @board[1].length
+        expect(row).to eq(6 )
+      end
+
+      it 'the board row value should be null' do
+        null = @board[1][1]
+        expect(null).to eq("\u26d4")
       end
 
       it 'should initialize two players with a unicode value, a black and white ball' do
@@ -44,14 +47,15 @@ describe Connect do
       before do
         @board = game.instance_variable_get(:@board)
         @player1 = game.instance_variable_get(:@player1)
+        @null = game.instance_variable_get(:@null)
       end
 
       it 'when picking column 1 change the last value of the array to the player' do
-        expect { game.place_player(1, @player1) }.to change { @board[1].last }.from(nil).to(@player1)
+        expect { game.place_player(1, @player1) }.to change { @board.values[0].values.last }.from(@null).to(@player1)
       end
 
       it 'when picking column 7 change the last value of the array to the player' do
-        expect { game.place_player(7, @player1) }.to change { @board[7].last }.from(nil).to(@player1)
+        expect { game.place_player(7, @player1) }.to change { @board.values[6 ].values.last }.from(@null).to(@player1)
       end
     end
 
@@ -59,15 +63,17 @@ describe Connect do
       before do
         @board = game.instance_variable_get(:@board)
         @player1 = game.instance_variable_get(:@player1)
+        @null = game.instance_variable_get(:@null)
         game.place_player(7, @player1)
       end
 
       it 'add the player to the second last position' do
-        expect { game.place_player(7, @player1) }.to change { @board[7][4] }.from(nil).to(@player1)
+        expect { game.place_player(7, @player1) }.to change { @board.values[6 ].values[-2] }.from(@null).to(@player1)
       end
 
-      it 'keeps the same length of 6 for the array' do
-        expect(@board[1].length).to eq(6)
+      it 'keeps the same length of 6  for the row' do
+        row = @board[1].length
+        expect(row).to eq(6 )
       end
     end
 
@@ -75,7 +81,7 @@ describe Connect do
       before do
         @board = game.instance_variable_get(:@board)
         @player1 = game.instance_variable_get(:@player1)
-        6.times { game.place_player(1, @player1) }
+        6 .times { game.place_player(1, @player1) }
       end
 
       it 'returns a message about the column being full' do
@@ -94,16 +100,97 @@ describe Connect do
 
   describe '#victory?' do
     let(:player1) { game.instance_variable_get(:@player1) }
+    let(:victory) { game.victory?(player1) }
+    context 'verctical victory' do
 
-    context 'first column has 4 in a row' do
-
-      before do
+      it 'return true for a vertical victory on the first column' do
         4.times { game.place_player(1, player1) }
+        expect(victory).to be(true)
       end
 
-      it 'return true for a victory' do
-        expect(game.victory?(player1)).to be(true)
+      it 'return true for a vertical victory on the last column' do
+        4.times { game.place_player(7, player1) }
+        expect(victory).to be(true)
+      end
+
+      it 'return nil when theres not victory on column 1' do
+        3.times { game.place_player(1, player1) }
+        expect(victory).to be(false)
+      end
+
+      it 'return nil when theres not victory on column 7' do
+        3.times { game.place_player(7, player1) }
+        expect(victory).to be(false)
+      end
+    end
+
+    context 'horizontal victory' do
+
+      it 'return true when theres a 4 in a row in the last row' do
+        count =  0
+        4.times { game.place_player(count += 1, player1) }
+        expect(victory).to eq(true)
+      end
+
+      it 'return false when theres no horizontal victory' do
+        count =  0
+        3.times { game.place_player(count += 1, player1) }
+        expect(victory).to eq(false)
+      end
+    end
+
+    context 'diagonal victory' do
+
+      before do
+        @board = game.instance_variable_get(:@board)
+      end
+
+      it 'returns true when theres a diagonal victory on the first column and first row' do
+        @board[1] = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6  =>'⚪' }
+        @board[2] = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 =>'⚪', 6  => '⚫' }
+        @board[3] = { 1 => 0, 2 => 0, 3 => 0, 4 => '⚪', 5 => '⚫', 6 => '⚫' }
+        @board[4] = { 1 => 0, 2 => 0, 3 => '⚪', 4 => '⚫', 5 => '⚫', 6 => '⚫' }
+        expect(victory).to eq(true)
+      end
+
+      it 'returns false when theres no diagonal victory' do
+        expect(victory).to eq(false)
+      end
+    end
+
+  end
+
+  describe '#vv?' do
+    context 'check vertical victory conditions' do
+      
+
+      before do
+        @player1 = game.instance_variable_get(:@player1)
+      end
+
+      it 'when given a hash counts the amount of times a player appears in the column if 4 return true' do
+        column = { 1 => 0, 2 => 0, 3 => @player1, 4 => @player1, 5 => @player1, 6 => @player1 }
+        victory = game.vv?(@player1, column)
+        expect(victory).to eq(true)
+      end
+
+      it 'when given a hash return nil if the count is not 4' do
+        column = { 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0 }
+        victory = game.vv?(@player1, column)
+        expect(victory).to eq(nil)
+      end
+    end
+
+  end
+
+  describe 'display_board' do
+    context 'every turn of the game' do
+      it 'puts 6  times board' do
+        expect(game).to receive(:puts).exactly(6 ).times
+        game.display_board
       end
     end
   end
+
 end
+
